5c147886644522601f1885e0b94c9f33521283b7
Atish Narlawar <atish.narlawar@gmail.com>
2014-08-10 23:49:44 -0400

    first commit

---
+++ HGDNData.h
@@ -0,0 +1,13 @@
+//
+//  HGDNData.h
+//  Daily Notes
+//
+//  Created by HUGE | Atish Narlawar on 8/9/14.
+//  Copyright (c) 2014 HugeInc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HGDNData : NSObject
+
+@end
---
+++ HGDNData.m
@@ -0,0 +1,13 @@
+//
+//  HGDNData.m
+//  Daily Notes
+//
+//  Created by HUGE | Atish Narlawar on 8/9/14.
+//  Copyright (c) 2014 HugeInc. All rights reserved.
+//
+
+#import "HGDNData.h"
+
+@implementation HGDNData
+
+@end
